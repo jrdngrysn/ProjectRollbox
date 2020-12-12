412bdfa640fe5b32041c72259b5855c49b06d7fa
Jordan Grayson <jtg400@nyu.edu>
2020-12-12 14:11:58 -0500

    Set up a quick level change, trying a stab at wind

---
+++ Assets/LevelChangeManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChangeManager : MonoBehaviour
+{
+
+    public string nextLevel;
+
+
+    public void NextLevel()
+    {
+        SceneManager.LoadScene(nextLevel);
+    }
+}
---
+++ Assets/WindManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindManager : MonoBehaviour
+{
+
+    public float raycastDistance = 5f;
+
+    private string playerTag;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit Hit;
+        Debug.DrawRay(gameObject.transform.position, gameObject.transform.right * raycastDistance, Color.green, 2);
+
+        if (Physics.Raycast(gameObject.transform.position, gameObject.transform.right * raycastDistance, out Hit, raycastDistance))
+        {
+            if (Hit.transform.gameObject.CompareTag(playerTag))
+            {
+
+            }
+        }
+    }
+}
