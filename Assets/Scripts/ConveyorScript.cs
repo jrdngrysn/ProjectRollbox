4fa7e8cc8de780d5cec65e6b28a1991121be169d
Jordan Grayson <jtg400@nyu.edu>
2020-12-17 15:46:33 -0500

    Updated conveyor and made a conveyor level, moved finished levels into folder

--- Assets/Scripts/ConveyorScript.cs
+++ Assets/Scripts/ConveyorScript.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ConveyorScript : MonoBehaviour
-{   
+{
+
+    [Header("Standard Settings")]
+    public GameObject usedButton;
+    ButtonScript bs;
 
     public float speed;
     private Vector2 direction;
 
+    [Header("Custom Settings")]
+    [Tooltip("If true, doesn't require a button to activate, and is 'on' the entire time.")]
+    public bool alwaysActive;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        bs = usedButton.GetComponent<ButtonScript>();
     }
 
     // Update is called once per frame
@@ -24,16 +32,20 @@ public class ConveyorScript : MonoBehaviour
 
     private void OnCollisionStay2D(Collision2D collision)
     {
-        Debug.Log("In Collision with: " + collision.gameObject.name);
-        direction = transform.right;
-        direction = direction * speed;
-
-        Rigidbody2D collisionRB = collision.gameObject.GetComponent<Rigidbody2D>();
-
-        if (collisionRB)
+        if (bs.buttonPressed || alwaysActive)
         {
-            Debug.Log("Found RigidBody2D");
-            collisionRB.AddForce(direction * collisionRB.mass);
+
+            Debug.Log("In Collision with: " + collision.gameObject.name);
+            direction = transform.right;
+            direction = direction * speed;
+
+            Rigidbody2D collisionRB = collision.gameObject.GetComponent<Rigidbody2D>();
+
+            if (collisionRB)
+            {
+                Debug.Log("Found RigidBody2D");
+                collisionRB.AddForce(direction * collisionRB.mass);
+            }
         }
     }
 
